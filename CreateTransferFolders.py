d550ae2017d24a6312757ff1c9f7ad025738b4b8
efheuston <efheuston@gmail.com>
2023-11-27 11:53:40 -0500

    Generalizing script for non-HPAP directory format

--- CreateTransferFolders.py
+++ CreateTransferFolders.py
@@ -10,33 +10,39 @@ from pathlib import Path
 import shutil
 
 
-
-
-hpap_dir = "/data/CRGGH/heustonef/hpapdata/cellranger_scRNA/"
+sc_dir = "/data/CRGGH/heustonef/hpapdata/cellranger_scRNA/"
 target_dir = "/data/CRGGH/heustonef/hpapdata/cellranger_scRNA/scRNA_transfer"
 target_files = ['web_summary.html', 'metrics_summary.csv']
 
-os.chdir(hpap_dir)
+search_term = 'EH00'
+sequencedata_type = "rna"
+
+
+os.chdir(sc_dir)
+
+# Compile search_term
+search_term = re.compile(search_term)
 
 # Get a list of samples to run
 sample_list = []
-for sampleDir in os.listdir(hpap_dir): # for each sample ID
-    if re.search('^HPAP-\d{3}_', sampleDir) is not None: # Make sure it follows the HPAP nomenclature
+for sampleDir in os.listdir(sc_dir): # for each sample ID
+    if re.search(search_term, sampleDir) is not None: 
         print(sampleDir)
         with open(os.path.join(sampleDir, "_cmdline"), 'r') as cmdline:
-            cellranger_cmd = cmdline.readline().split(' ')[0]
-            if re.search('cellranger-atac', cellranger_cmd):
-                sequencedata_type = 'atac'
-                filteredData_dir = "filtered_peak_bc_matrix"
-                target_files = target_files + ['filtered_peak_bc_matrix.h5', 'barcodes.tsv', 'matrix.mtx', 'peaks.bed']
-                print("Reading ATAC files")
-            elif re.search('cellranger$', cellranger_cmd):
-                sequencedata_type = 'rna'
-                filteredData_dir = "filtered_feature_bc_matrix"
-                target_files = target_files + ['filtered_feature_bc_matrix.h5', 'barcodes.tsv.gz', 'features.tsv.gz', 'matrix.mtx.gz']
-                print("Reading RNA files")
-            else:
-                print("Could not determine sequencedata_type")
+            if sequencedata_type is not None:
+                cellranger_cmd = cmdline.readline().split(' ')[0]
+                if re.search('cellranger-atac', cellranger_cmd):
+                    sequencedata_type = 'atac'
+                    filteredData_dir = "filtered_peak_bc_matrix"
+                    target_files = target_files + ['filtered_peak_bc_matrix.h5', 'barcodes.tsv', 'matrix.mtx', 'peaks.bed']
+                    print("Reading ATAC files")
+                elif re.search('cellranger$', cellranger_cmd):
+                    sequencedata_type = 'rna'
+                    filteredData_dir = "filtered_feature_bc_matrix"
+                    target_files = target_files + ['filtered_feature_bc_matrix.h5', 'barcodes.tsv.gz', 'features.tsv.gz', 'matrix.mtx.gz']
+                    print("Reading RNA files")
+                else:
+                    print("Could not determine sequencedata_type")
         target_path = os.path.join(target_dir, sampleDir, "outs", filteredData_dir)
         Path(target_path).mkdir(parents = True, exist_ok = True)
         # Copy run information
@@ -45,11 +51,11 @@ for sampleDir in os.listdir(hpap_dir): # for each sample ID
         # Copy target files
         for dirpath, subdirs, files in os.walk(sampleDir):
             for file in files:
-                if file in target_files and  dirpath in target_path:
+                if re.search(file, target_files) and  dirpath in target_path:
                     shutil.copy2(os.path.join(dirpath, file), os.path.join(target_dir, dirpath, file))
-                    if file == "web_summary.html":
+                    if re.search(file, "web_summary.html") and not file == "web_summary.html":
                         os.rename(os.path.join(target_dir, dirpath, file), os.path.join(target_dir, dirpath,''.join((sampleDir, '-', file))))
-                    if file == "metrics_summary.csv":
+                    if re.search(file, "metrics_summary.csv") and not file == "metrics_summary.csv":
                         os.rename(os.path.join(target_dir, dirpath, file), os.path.join(target_dir, dirpath,''.join((sampleDir, '-', file))))
 
 
