9e31ff89f5d39afaad26064a4380e334aa5b51f1
Elisabeth Heuston <heustonef@nih.gov>
2025-12-12 08:45:56 -0500

    updated and made slightly more pythonic

--- CellBender.py
+++ CellBender.py
@@ -1,69 +1,64 @@
-# 2023.11.30 CellBender Script
-# Assumes you're using raw_feature_bc_matrix.h5 files output from CellRanger Count (v7.2.0 as of 2023.11.30)
+# 2025.12.11 CellBender Script
+# Assumes you're using raw_feature_bc_matrix.h5 files output from CellRanger Count (v10.0.0 as of 2025.12.11)
 # IO https://cellbender.readthedocs.io/en/latest/usage/index.html
-
-# conda create -n cellbender python=3.7
 
 
 # Import modules
 import os
-import re
-# import subprocess
+from collections import defaultdict
 
 # Define global variables
 
 #swarm -f cellbender.swarm -g 64 -t 8 --time=24:00:00 --merge-output --module cellbender --sbatch "--mail-type=BEGIN,END,FAIL"
-cellranger_folder = "/data/CRGGH/heustonef/hpapdata/cellranger_scRNA/" # Required: folder containing cellranger output data
+cellranger_folder = "/data/CRGGH/heustonef/huMuscle/aadm_x18/" # Required: folder containing cellranger output data
 cellbender_file = "cellbender.swarm" # Optional: None, or name of file to write cellbender commands. Useful if submitting as swmarm file
 gpu_run = False
-flags = "--cpu-threads 4" # Optional: CellBender flags to include in command
-ignore_folders = "SC_RNA_COUNTER_CS"
+flags = "--cpu-threads $SLURM_CPUS_PER_TASK" # Optional: CellBender flags to include in command
+
+swarm_statement=f"""
+#swarm -f {cellbender_file} -g 64 -t 8 --time=24:00:00 --merge-output --module cellbender --sbatch "--mail-type=BEGIN,END,FAIL"
+"""
+
+
 
 if cellbender_file:
 	cellbender_file = os.path.join(cellranger_folder, cellbender_file)
+	with open(cellbender_file, 'w') as cellbender:
+		cellbender.write(swarm_statement)
 if gpu_run is True:
 		flags = ''.join((" --cuda ", flags))
 
 # Create list of raw_feature_bc_matrix.h5 files
-sample_list = []
-for root, _, files in os.walk(cellranger_folder):
-	if ignore_folders in root:
-		continue
-	else:
-		for file in files:
-			if re.match("raw_feature_bc_matrix.h5", file):
-				sample_list.append(os.path.join(root, file))
-				print(''.join(("Found ", os.path.join(root, file), "\n")))
+sample_list = defaultdict(dict)
+for sampleID in os.listdir(cellranger_folder):
+    sample_path = os.path.join(cellranger_folder, sampleID)
+    if not os.path.isdir(sample_path):
+        continue  # skip non-directories
 
-# Run CellBender
+    outs_dir = os.path.join(sample_path, "outs")
+    if not os.path.isdir(outs_dir):
+        continue  # skip samples without outs
 
-for cellbender_input in sample_list:
-	export_folder = os.path.dirname(cellbender_input)
-	cellbender_output = os.path.basename(cellbender_input).replace("raw", "cb")
-	cellbender_cmd = ''.join(( 
-		"cellbender remove-background ",
-		flags,
-		" --input ",
-		cellbender_input,
-		" --output ",
-		os.path.join(export_folder, cellbender_output), 
-		'; \\\n'
-	))
-	print(cellbender_cmd)
-	if cellbender_file:
-		cd_cmd = ''.join(("cd ", export_folder, '; \\\n'))
-		with open(cellbender_file, 'a') as cellbender_out:
-			cellbender_out.write(cd_cmd)
-			cellbender_out.write(cellbender_cmd)
-			cellbender_out.write('ptrepack --complevel 5 cb_feature_bc_matrix_filtered.h5:/matrix cb-seurat_feature_bc_matrix_filtered.h5:/matrix')
-			cellbender_out.write('\n\n')
-	elif cellbender_file == None:
-		print("running cellbender on files")
-		# subprocess.run(cellbender_cmd, capture_output=True)
-	
-# note: to process through seurat, need to reformat using PyTables
-#  ptrepack --complevel 5 cb_feature_bc_matrix_filtered.h5:/matrix cb-seurat_feature_bc_matrix_filtered.h5:/matrix	
+    # Now look directly in the outs folder
+    for fname in os.listdir(outs_dir):
+        if fname == "raw_feature_bc_matrix.h5":
+            matrix_path = os.path.join(outs_dir, fname)
+            sample_list[sampleID][matrix_path] = os.path.join(
+                outs_dir, "cb_feature_bc_matrix.h5"
+            )
 
+for sampleID, mapping in sorted(sample_list.items()):
+	for paths, outfile in mapping.items():
+		cellbender_cmd=f"""#Sample {sampleID}
+cd {paths}; \\
+cellbender remove-background {flags} \\
+--input {paths} \\
+--output {outfile}; \\
+ptrepack --complevel 5 cb_feature_bc_matrix_filtered.h5:/matrix cb-seurat_feature_bc_matrix_filtered.h5:/matrix
+
+"""
+	with open (cellbender_file, 'a') as cellbender:
+		cellbender.write(cellbender_cmd)
 
 print("Done")
 
