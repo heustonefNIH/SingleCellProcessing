fdb7187632eea9855b06874bbaf9752b650b843c
Elisabeth Heuston <heustonef@nih.gov>
2026-03-24 08:54:55 -0400

    Fixed bug to check that filenames end with _001.fastq.gz

--- fx_hpap_rename.py
+++ fx_hpap_rename.py
@@ -12,6 +12,7 @@ def hpap_rename(fname, dirpath, run_mode):
     new_name = re.sub('_fastq-data', '', new_name)
     new_name = re.sub('_10xscRNA_', '_', new_name)
     new_name = re.sub(r'HPAP(\d{3})', r'HPAP-\1', new_name)
+    new_name = re.sub(r'_(R1|R2|I1|I2)\.fastq\.gz$', r'_\1_001.fastq.gz', new_name)
 
     src = os.path.join(dirpath, fname)
     dst = os.path.join(dirpath, new_name)
@@ -27,11 +28,12 @@ def hpap_rename(fname, dirpath, run_mode):
     if not new_name == fname:
         logger.info("Attempting to rename %s -> %s", src, dst)
 
-    if os.path.exists(dst):
-        logger.error("Cannot rename %s to %s because destination already exists.", src, dst)
-        return None
     if src == dst:
         logger.debug("Source and destination are identical: %s", src)
+        return None
+
+    if os.path.exists(dst):
+        logger.error("Cannot rename %s to %s because destination already exists.", src, dst)
         return None
 
     if run_mode:
